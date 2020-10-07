0f020d380562482a664033614f9b41112ace96a8
skyrippa <iagob4599@gmail.com>
2020-10-07 15:14:10 -0300

    add service and repository layers

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Repositories\ClientRepository;
+use CodeProject\Services\ClientService;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * @var ClientRepository
+     */
+    private $repository;
+    /**
+     * @var ClientService
+     */
+    private $service;
+
+    public function __construct(ClientRepository $repository, ClientService $service)
+    {
+        $this->repository = $repository;
+
+        $this->service = $service;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->repository->all();
+    }
+
+    public function store(Request $request)
+    {
+        return $this->service->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return mixed
+     */
+    public function show($id)
+    {
+        return $this->repository->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return void
+     */
+    public function update(Request $request, $id)
+    {
+        return $this->service->update($request->all(), $id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return void
+     */
+    public function destroy($id)
+    {
+        $this->repository->find($id)->delete();
+    }
+}
